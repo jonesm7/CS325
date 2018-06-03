a3a5dad213778ec8e743aa5580f69a18615044ba
Mindy Jones <cherrytomato.jones@gmail.com>
2018-06-03 08:02:23 -0700

    Adding 2-opt

--- RunTSP.cpp
+++ RunTSP.cpp
@@ -35,6 +35,108 @@ void nearestNeighborQuadTree(const vector<Node*>& cities, vector<Node*>& tour) {
 	}
 }
 
+int rounded(double val) {
+	return val + 0.5;
+}
+
+/****************************************************************************
+**                             twoOptImprove                               **
+** This function receives a tour tuple (tsp solution) and a vector graph   **
+** (see 'loadMapAsGraphOfVectors' above), and attempts to restructure the  **
+** the tour by 'swapping' eligible pairs of edges, if said swap reduces    **
+** the total tour distance. This is in attempt to eliminate path cross-    **
+** over that contributes to sub-optimality. When a swap occurs, the path   **
+** in between the vertices swapped is reversed,to maintain the tour        **
+** integrity. See https://en.wikipedia.org/wiki/2-opt and                  **
+** http://pedrohfsd.com/2017/08/09/2opt-part1.html for more details.       **
+****************************************************************************/
+void twoOptImprove(vector<Node*>& tspTour)
+{
+	//This variable (breakOutToOptimize) is set to allow the loop to repeat
+	//until the optimal improvement is obtained for small data sizes (n <= 2500).
+	//In this case, execution exits both the inner and outer loops when each swap
+	//is made, and the process repeats until no further improvement is possible.
+	//For large data sets, the improvement runs only once (all the way through) to 
+	//ensure a reasonable running time (at the expense of optimality). 
+	//(See related lines 321-324 and note the control statement 
+	//`breakOutToOptimize == false` in both the inner and outer for loops.)
+	bool breakOutToOptimize;
+	bool nExceeds2500;
+	bool improved;
+	if (tspTour.size() > 2500)
+	{
+		nExceeds2500 = true;
+	}
+	else
+	{
+		nExceeds2500 = false;
+	}
+
+	do
+	{
+		improved = false;
+		breakOutToOptimize = false;
+		//(Can't swap 1st city so i starts at 1...)
+		for (int i = 1; i < tspTour.size() - 2 &&
+			breakOutToOptimize == false; i++)
+		{
+			for (int j = i, k = i + 1; k < tspTour.size() &&
+				breakOutToOptimize == false; k++)
+			{
+				//Adjacent vertices are not eligible for consideration
+				//because there is only one edge between them.
+				if (k - j == 1)
+				{
+					continue;
+				}
+
+				//If distance(i to i + j -1) + distance(i + 1, j) <
+				//   distance(i to i + 1) + distance(j - 1 to j), the swap
+				//will improve the tour. In other words, if taking out the two
+				//edges before the swap and inserting two new edges (because of swap)
+				//results in shorter tour, the cities are swapped in tour order.
+				if (rounded(tspTour[j]->loc.distTo(tspTour[k - 1]->loc)) +
+					rounded(tspTour[j + 1]->loc.distTo(tspTour[k]->loc)) <
+					rounded(tspTour[j]->loc.distTo(tspTour[j + 1]->loc)) +
+					rounded(tspTour[k - 1]->loc.distTo(tspTour[k]->loc)))
+				{
+
+					improved = true;
+					//Only need to reverse cities in between swapped routes (edges).
+					for (int l = j + 1, m = k - 1; l < m; l++, m--)
+					{
+						Node* temp = tspTour[l];
+						tspTour[l] = tspTour[m];
+						tspTour[m] = temp;
+					}
+					//Allow optimization of improvement if data size is manageable.
+					//(Otherwise, additional time cost is unreasonable, run 2Opt-swap once over only.)
+					//If enabled (i.e. data set <= 1000), execution exits both inner and outer
+					//loop after improvement to start over.
+					if (!nExceeds2500)
+					{
+						breakOutToOptimize = true;
+					}
+				}
+			}
+		}
+	} while (improved);
+}
+
+int getTourLength(const vector<Node*>& tour) {
+	int length = 0;
+	for (int i = 0; i < tour.size(); i++) {
+		int compareIdx = i + 1;
+		if (compareIdx == tour.size()) {
+			compareIdx = 0;
+		}
+		Point p1 = tour[i]->loc;
+		Point p2 = tour[compareIdx]->loc;
+		length += rounded(p1.distTo(p2));
+	}
+	return length;
+}
+
 void printTour(const vector<Node*>& tour, string outputFileBase) {
 	int length = 0;
 	for (int i = 0; i < tour.size(); i++) {
@@ -99,7 +201,18 @@ int main(int argc, char * argv[])
 	nearestNeighborQuadTree(cities, tour);
 	high_resolution_clock::time_point t2 = high_resolution_clock::now();
 	auto duration = duration_cast<microseconds>(t2 - t1).count() / 1000000.0f;
-	cout << "duration: " << duration << " seconds" << endl;
+	cout << "tour construction: " << duration << " seconds" << endl;
+
+	high_resolution_clock::time_point t3 = high_resolution_clock::now();
+	twoOptImprove(tour);
+	high_resolution_clock::time_point t4 = high_resolution_clock::now();
+	auto duration2 = duration_cast<microseconds>(t4 - t3).count() / 1000000.0f;
+	cout << "tour optimization: " << duration2 << " seconds" << endl;
+
+	auto durationTotal = duration_cast<microseconds>(t4 - t1).count() / 1000000.0f;
+	cout << "total time: " << durationTotal << " seconds" << endl;
+
+	cout << "tour length: " << getTourLength(tour) << endl;
 
 	printTour(tour, inFilename);
 
